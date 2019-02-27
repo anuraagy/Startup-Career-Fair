823a8efda43a1f4954bb4497459f9992cbdd1cb8
Anuraag Yachamaneni <ayachama@purdue.edu>
2019-02-27 18:43:16 -0500

    Update logos

--- dist/js/companies.js
+++ dist/js/companies.js
@@ -1,27 +1,24 @@
 companies = [
   { logo: "amerlight", url: "https://www.amerlight.com" },
   { logo: "asimpleart", url: "https://asimpleart.com" },
-  { logo: "babyhop", url: "https://babyhop.co/" },
   { logo: "bloomerang", url: "https://bloomerang.co/" },
+  { logo: "bolstra", url: "https://bolstra.com/" },
   { logo: "canceriq", url: "https://www.canceriq.com" },
   { logo: "cooler", url: "https://www.coolerscreens.com" },
   { logo: "crafty", url: "https://craftydelivers.com" },
   { logo: "crema", url: "https://crema.us" },
-  { logo: "demandjump", url: "https://demandjump.com" },
   { logo: "doublemap", url: "https://www.doublemap.com" },
   { logo: "fillintime", url: "https://fillintimellc.wixsite.com/fill-in-time" },
   { logo: "freeosk", url: "https://www.thefreeosk.com/" },
-  { logo: "halo", url: "https://haloinvesting.com"},
   { logo: "ifi", url: "https://opp.purdue.edu/programs/Interns_for_Indiana.html" },
   { logo: "inari", url: "http://inari.com/" },
   { logo: "iter8", url: "http://iter8.launchrock.com/" },
   { logo: "look", url: "https://look.education" },
   { logo: "monumentlabs", url: "https://getmonument.com/" },
-  { logo: "onecause", url: "https://www.onecause.com" },
   { logo: "panopta", url: "https://www.panopta.com/" }, 
   { logo: "pathmaker", url: "https://www.itap.purdue.edu/pathmaker/" },
-  { logo: "planeenglish", url: "https://planeenglish.net/" },
   { logo: "playlister", url: "https://WWW.PLAYLISTER.APP" },
+  { logo: "printwithme", url: "https://www.printwithme.com/" },
   { logo: "proglove", url: "https://proglove.com" },
   { logo: "rogo", url: "https://rogoag.com" },
   { logo: "silverline", url: "https://silverline.group"},
@@ -29,7 +26,6 @@ companies = [
   { logo: "teacherly", url: "https://teacher.ly/" },
   { logo: "thickstat", url: "https://www.thickstat.com" },
   { logo: "zWorks", url: "https://www.zworks.org/" },
-  { logo: "zylo", url: "https://zylo.com/" }
 ]
 
 
